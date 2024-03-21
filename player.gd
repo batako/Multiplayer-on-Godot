d950da2a162d465f60411d41aa39f0fdc96b76e6
batako <guri725ren213@gmail.com>
2024-03-22 00:35:03 +0900

    プレイヤーの位置の同期するサンプル追加

---
+++ player.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+
+@export var sync_position := Vector2.ZERO
+
+const SPEED = 300.0
+
+
+func is_local_authority() -> bool:
+	return name == str(multiplayer.get_unique_id())
+
+
+func _physics_process(delta):
+	if not is_local_authority():
+		position = sync_position
+		return
+	
+	if Input.is_action_pressed("ui_left"):
+		position.x -= delta * SPEED
+	if Input.is_action_pressed("ui_right"):
+		position.x += delta * SPEED
+	if Input.is_action_pressed("ui_up"):
+		position.y -= delta * SPEED
+	if Input.is_action_pressed("ui_down"):
+		position.y += delta * SPEED
+	
+	rpc_id(1, StringName("push_to_server"), position)
+
+
+@rpc("any_peer", "unreliable_ordered")
+func push_to_server(_sync_pos: Vector2):
+	if not multiplayer.is_server():
+		return
+	if name != str(multiplayer.get_remote_sender_id()):
+		print("someone being naughty!", multiplayer.get_remote_sender_id(), ' tried to update ', name)
+		return
+	sync_position = _sync_pos
+	
---
+++ world.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+
+@export var PlayerScene: PackedScene
+@export var host: String = "localhost"
+@export var port: int = 3000
+
+@onready var reconnect_timer: Timer = $ReconnectTimer
+@onready var net_work_status_label: Label = $NetworkStatusLabel
+@onready var networked_nodes: Node = $NetworkedNodes
+
+
+var peer: ENetMultiplayerPeer
+
+func _ready():
+	start_network()
+
+
+func _on_reconnect_timer_timeout():
+	var message = ""
+
+	if peer.get_connection_status() == ENetMultiplayerPeer.CONNECTION_DISCONNECTED or peer.get_connection_status() == ENetMultiplayerPeer.CONNECTION_CONNECTING:
+		peer.create_client(host, port)
+		message = "切断されています。再接続を試みます..."
+		net_work_status_label.modulate = Color(1, 0, 0, 1)
+	elif peer.get_connection_status() == ENetMultiplayerPeer.CONNECTION_CONNECTED:
+		message = "サーバに接続済みです。"
+		net_work_status_label.modulate = Color(0, 1, 0, 1)
+	
+	net_work_status_label.text = message
+
+
+func start_network() -> void:
+	peer = ENetMultiplayerPeer.new()
+	if "--server" in OS.get_cmdline_args():
+		multiplayer.peer_connected.connect(create_player)
+		multiplayer.peer_disconnected.connect(destroy_player)
+		peer.create_server(port)
+		print("server listening on localhost " + str(port))
+
+	else:
+		connect_to_server()
+
+	multiplayer.multiplayer_peer = peer
+
+
+func connect_to_server() -> void:
+	peer.create_client(host, port)
+	reconnect_timer.start()
+
+
+func create_player(id: int) -> void:
+	print(str(id) + " が接続しました。")
+	var player = PlayerScene.instantiate()
+	player.name = str(id)
+	networked_nodes.add_child(player)
+
+
+func destroy_player(id: int) -> void:
+	print(str(id) + " が切断しました。")
+	networked_nodes.get_node(str(id)).queue_free()
